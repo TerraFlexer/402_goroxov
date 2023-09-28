105288480b767f39b03e23eeb7684b635d2ae9fa
Sergey Gorohov <gamehouse02@yandex.ru>
2023-09-28 21:56:59 +0300

    First add

---
+++ Apploger/NUGETAIRATLOX/Class1.cs
@@ -0,0 +1,129 @@
+﻿using BERTTokenizers;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+namespace NuGetAnswering
+{
+    public class BertModel
+    {
+        private static InferenceSession session;
+        private static string modelUrl;
+        private static string modelPath;
+        private readonly object locker = new object();
+        static public Queue<string> progress = new Queue<string>();
+        CancellationToken cancelToken;
+        public BertModel(string modelUrlTemp, string modelPathTemp, CancellationToken cancelTokenTemp = default)
+        {
+            modelPath = modelPathTemp;
+            modelUrl = modelUrlTemp;
+            cancelToken = cancelTokenTemp;
+
+        }
+        public async Task Create()
+        {
+            if (!File.Exists(modelPath))
+            {
+                await DownloadModelWithRetryAsync();
+            }
+            session = new InferenceSession(modelPath);
+        }
+
+
+        public Task<string> GetAnswerAsync(string text, string question)
+        {
+            return Task.Factory.StartNew(() => {
+                try
+                {
+                    cancelToken.ThrowIfCancellationRequested();
+                    var sentence = "{\"question\": \"" + question + "\", \"context\": \"@CTX\"}".Replace("@CTX", text);
+                    var tokenizer = new BertUncasedLargeTokenizer();
+                    var tokens = tokenizer.Tokenize(sentence);
+                    var encoded = tokenizer.Encode(tokens.Count(), sentence);
+                    var bertInput = new BertInput()
+                    {
+                        InputIds = encoded.Select(t => t.InputIds).ToArray(),
+                        AttentionMask = encoded.Select(t => t.AttentionMask).ToArray(),
+                        TypeIds = encoded.Select(t => t.TokenTypeIds).ToArray(),
+                    };
+
+                    var input_ids = ConvertToTensor(bertInput.InputIds, bertInput.InputIds.Length);
+                    var attention_mask = ConvertToTensor(bertInput.AttentionMask, bertInput.InputIds.Length);
+                    var token_type_ids = ConvertToTensor(bertInput.TypeIds, bertInput.InputIds.Length);
+                    var input = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("input_ids", input_ids),
+                                                            NamedOnnxValue.CreateFromTensor("input_mask", attention_mask),
+                                                            NamedOnnxValue.CreateFromTensor("segment_ids", token_type_ids) };
+
+                    cancelToken.ThrowIfCancellationRequested();
+                    IDisposableReadOnlyCollection<DisposableNamedOnnxValue>? output;
+                    lock (session)
+                    {
+                        output = session.Run(input);
+                    }
+                    cancelToken.ThrowIfCancellationRequested();
+
+                    List<float> startLogits = (output.ToList().First().Value as IEnumerable<float>).ToList();
+                    List<float> endLogits = (output.ToList().Last().Value as IEnumerable<float>).ToList();
+
+                    var startIndex = startLogits.ToList().IndexOf(startLogits.Max());
+                    var endIndex = endLogits.ToList().IndexOf(endLogits.Max());
+                    var predictedTokens = tokens
+                                .Skip(startIndex)
+                                .Take(endIndex + 1 - startIndex)
+                                .Select(o => tokenizer.IdToToken((int)o.VocabularyIndex))
+                                .ToList();
+
+                    var answer = String.Join(" ", predictedTokens);
+                    cancelToken.ThrowIfCancellationRequested();
+                    return answer;
+                }
+                catch (OperationCanceledException) { return "Operation was cancelled"; }
+                catch (Exception ex) { return ex.Message; }
+            }, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+
+        }
+        public async Task DownloadModelWithRetryAsync()
+        {
+            int maxr = 5, ind = 0;
+            while (ind < maxr)
+            {
+                try
+                {
+                    var client = new HttpClient();
+                    client.Timeout = TimeSpan.FromMinutes(5);
+                    var response = await client.GetAsync(modelUrl);
+                    response.EnsureSuccessStatusCode();
+                    using var stream = await client.GetStreamAsync(modelUrl);
+                    using var fileStream = new FileStream(modelPath, FileMode.CreateNew);
+                    await stream.CopyToAsync(fileStream);
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    ind ++;
+                    throw;
+                }
+            }
+        }
+
+        public class BertInput
+        {
+            public long[] InputIds { get; set; }
+            public long[] AttentionMask { get; set; }
+            public long[] TypeIds { get; set; }
+        }
+        public static Tensor<long> ConvertToTensor(long[] inputArray, int inputDimension)
+        {
+            Tensor<long> input = new DenseTensor<long>(new[] { 1, inputDimension });
+            for (var i = 0; i < inputArray.Length; i++)
+            {
+                input[0, i] = inputArray[i];
+            }
+            return input;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using NuGetAnswering;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Please specify file in arguments");
+            return;
+        }
+
+        string FilePath = args[0];
+        /*FilePath = "C:\\Users\\gameh\\source\\repos\\Apploger\\Apploger\\hobbit.txt";*/
+
+        string text = ReadFile(FilePath);
+        Console.WriteLine(text);
+        CancellationTokenSource cts = new CancellationTokenSource();
+        CancellationToken cancelToken = cts.Token;
+        string modelUrl = "https://storage.yandexcloud.net/dotnet4/bert-large-uncased-whole-word-masking-finetuned-squad.onnx";
+        string modelPath = "bert-large-uncased-whole-word-masking-finetuned-squad.onnx";
+        var answerTask = new BertModel(modelUrl, modelPath, cancelToken);
+        await answerTask.Create();
+        var tasks = new List<Task>();
+        while (!cancelToken.IsCancellationRequested)
+        {
+            Console.Write("Ask a question or press enter to exit: ");
+            string question = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(question))
+                cts.Cancel();
+
+            var task = answerTask.GetAnswerAsync(text, question).ContinueWith(task => { Console.WriteLine(question + " : " + task.Result); });
+            tasks.Add(task);
+
+        }
+        await Task.WhenAll(tasks);
+
+
+    }
+
+    static string ReadFile(string path)
+    {
+        StreamReader reader = null;
+        try
+        {
+            reader = new StreamReader(path);
+            return reader.ReadToEnd();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception loading from file: {ex.Message}");
+            return null;
+        }
+        finally
+        {
+            if (reader != null)
+            {
+                reader.Dispose();
+            }
+        }
+    }
+}
